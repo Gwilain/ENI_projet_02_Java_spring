f956418cc08cabb6fe2fde2c96a8bac7df9ed1d1
Ghislain Gillet <ghislain.gillet@gmail.com>
2025-06-11 12:12:43 +0200

    create utilisateur

---
+++ src/main/java/fr/eni/encheres/exception/ServiceExceptionCode.java
@@ -0,0 +1,8 @@
+package fr.eni.encheres.exception;
+
+public class ServiceExceptionCode {
+
+    public static final String PSEUDO_EXIST= "LE pseudo existe déjà";
+    public static final String EMAIL_EXIST = "L email existe déjà";
+
+}
---
+++ src/main/java/fr/eni/encheres/repository/UtilisateurRepository.java
@@ -0,0 +1,11 @@
+package fr.eni.encheres.repository;
+
+import fr.eni.encheres.model.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository  // facultatif, Spring détecte automatiquement les interfaces JpaRepository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByPseudo(String pseudo);
+}
